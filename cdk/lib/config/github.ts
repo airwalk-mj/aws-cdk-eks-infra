22bfd03ae74628503629b3848877bc8b06f8a728
Mark James <59691114+airwalk-mj@users.noreply.github.com>
2024-01-04 19:37:39 +0000

    fixed url

--- cdk/lib/config/github.ts
+++ cdk/lib/config/github.ts
@@ -1,6 +1,7 @@
 import { env } from "process"; 
 
+// https://eu-west-1.console.aws.amazon.com/codesuite/settings/connections?region=eu-west-1&connections-meta=eyJmIjp7InRleHQiOiIifSwicyI6e30sIm4iOjIwLCJpIjowfQ
 export class GithubConfig {
     static readonly GITHUB_REPO: string = "airwalk-mj/eks-infra-single-cluster";
-    static readonly GITHUB_CODESTAR_CONNECTION_ARN = "arn:aws:codestar-connections:eu-west-1:544294979223:connection/2d198d41-b2db-42d8-a752-6b941b1906a3";
+    static readonly GITHUB_CODESTAR_CONNECTION_ARN = "arn:aws:codestar-connections:eu-west-1:544294979223:connection/29d4d875-8eaa-4e19-ad9f-8dcb8f0cce19";
 }
