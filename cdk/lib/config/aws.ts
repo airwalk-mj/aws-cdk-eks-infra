2bede3621afc780fb52c1a1875e865af767dd1a9
Mark James <59691114+airwalk-mj@users.noreply.github.com>
2024-01-04 18:35:34 +0000

    aws account info updated

--- cdk/lib/config/aws.ts
+++ cdk/lib/config/aws.ts
@@ -1,6 +1,6 @@
 import { env } from "process";
 
 export class AwsConfig {
-    static readonly DEPLOYMENT_STACK_ACCOUNT = "825625126603";
-    static readonly DEPLOYMENT_STACK_REGION = "us-west-2";
+    static readonly DEPLOYMENT_STACK_ACCOUNT = "544294979223";
+    static readonly DEPLOYMENT_STACK_REGION = "eu-west-1";
 }
