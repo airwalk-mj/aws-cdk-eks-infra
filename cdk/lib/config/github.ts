766417aab6c7d09651aea12092529c7bd5a76818
Mark James <59691114+airwalk-mj@users.noreply.github.com>
2024-01-04 19:08:15 +0000

    fixed GITHUB_REPO

--- cdk/lib/config/github.ts
+++ cdk/lib/config/github.ts
@@ -1,6 +1,6 @@
 import { env } from "process"; 
 
 export class GithubConfig {
-    static readonly GITHUB_REPO: string = "airwalk-mj/aws-cdk-eks-infra";
+    static readonly GITHUB_REPO: string = "airwalk-mj/eks-infra-single-cluster";
     static readonly GITHUB_CODESTAR_CONNECTION_ARN = "arn:aws:codestar-connections:eu-west-1:544294979223:connection/2d198d41-b2db-42d8-a752-6b941b1906a3";
 }
